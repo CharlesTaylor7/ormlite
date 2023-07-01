60bd2dc5ea2ff925ee789b1d0d929bc274e49691
Charles Taylor <charlestaylor95@gmail.com>
2023-07-01 15:01:09 -0400

    Test edge case

--- src/spec/test_orm.py
+++ src/spec/test_orm.py
@@ -1,9 +1,15 @@
 import pytest
 
-from ormlite import model
+from ormlite import model, field
 
 def test_bare_model_decorator_is_not_supported():
     with pytest.raises(Exception):
         @model
         class Foo:
             pass
+
+def test_invalid_foreign_key():
+    with pytest.raises(Exception):
+        @model('foos')
+        class Foo:
+            fk: field(fk="three.part.fk")
