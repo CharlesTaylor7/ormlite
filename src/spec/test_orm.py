a52b9aedb3fcacd792212933b360db810825fe8b
Charles Taylor <charlestaylor95@gmail.com>
2023-07-01 15:04:28 -0400

    Test edge case

--- src/spec/test_orm.py
+++ src/spec/test_orm.py
@@ -1,12 +1,25 @@
 import pytest
+import dataclasses as dc
 
 from ormlite import model, field
+from .utils import unregister_all_models
 
 def test_bare_model_decorator_is_not_supported():
     with pytest.raises(Exception):
         @model
         class Foo:
             pass
+
+def test_foreign_key_punning():
+    @model('foos')
+    class Foo:
+        fk: str = field(fk="bars")
+
+    @model('bars')
+    class Foo:
+        fk: str
+
+    assert dc.fields(Foo) = []
 
 def test_invalid_foreign_key():
     with pytest.raises(Exception):
